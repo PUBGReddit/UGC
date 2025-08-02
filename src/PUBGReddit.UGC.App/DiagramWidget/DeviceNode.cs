abed0f98c307f11b2d70d68c898423cb312e1da5
Delzaugh <Delzaugh@gmail.com>
2025-08-02 09:43:04 +0200

    svg testings

---
+++ src/PUBGReddit.UGC.App/DiagramWidget/DeviceNode.cs
@@ -0,0 +1,10 @@
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Models;
+
+namespace PUBGReddit.UGC.App.DiagramWidget;
+public class DeviceNode : SvgNodeModel
+{
+    public DeviceNode(Point? position = null) : base(position) { }
+
+    // Here, you can put whatever you want
+}
--- src/PUBGReddit.UGC.App/Pages/WeatherB.razor.cs
+++ src/PUBGReddit.UGC.App/Pages/WeatherB.razor.cs
@@ -1,40 +1,57 @@
 using Blazor.Diagrams;
 using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Controls.Default;
 using Blazor.Diagrams.Core.Geometry;
 using Blazor.Diagrams.Core.Models;
 using Blazor.Diagrams.Core.PathGenerators;
+using Blazor.Diagrams.Core.Positions;
 using Blazor.Diagrams.Core.Routers;
 using Microsoft.AspNetCore.Components;
+using PUBGReddit.UGC.App.DiagramWidget;
 
 namespace PUBGReddit.UGC.App.Pages
 {
     public class WeatherComponent : ComponentBase
     {
-       protected readonly BlazorDiagram BlazorDiagram = new();
+        protected readonly BlazorDiagram BlazorDiagram = new();
 
         protected override void OnInitialized()
         {
             base.OnInitialized();
 
+            BlazorDiagram.RegisterComponent<AirDropNode, AirDropWidget>();
+            BlazorDiagram.RegisterComponent<DeviceNode, DeviceWidget>();
             var node1 = NewNode(50, 50);
-            var node2 = NewNode(300, 300);
-            var node3 = NewNode(300, 50);
+            var node2 = NewDeviceNode(300, 300);
+            var node3 = NewDeviceNode(300, 50);
             BlazorDiagram.Nodes.Add(node1);
             BlazorDiagram.Nodes.Add(node2);
             BlazorDiagram.Nodes.Add(node3);
 
-            BlazorDiagram.Links.Add(new LinkModel(node1.GetPort(PortAlignment.Right), node2.GetPort(PortAlignment.Left))
+            var Link1 = new LinkModel(node1.GetPort(PortAlignment.Right), node2.GetPort(PortAlignment.Left))
             {
-                SourceMarker = LinkMarker.Arrow,
-                TargetMarker = LinkMarker.Arrow
-            });
-            BlazorDiagram.Links.Add(new LinkModel(node2.GetPort(PortAlignment.Right), node3.GetPort(PortAlignment.Right))
-            {
-                Router = new OrthogonalRouter(),
+                Router = new NormalRouter(),
                 PathGenerator = new StraightPathGenerator(),
                 SourceMarker = LinkMarker.Arrow,
                 TargetMarker = LinkMarker.Arrow
-            });
+            };
+            var Link2 = new LinkModel(node2.GetPort(PortAlignment.Right), node3.GetPort(PortAlignment.Left))
+            {
+                Router = new NormalRouter(),
+                PathGenerator = new StraightPathGenerator(),
+                SourceMarker = LinkMarker.Arrow,
+                TargetMarker = LinkMarker.Arrow
+            };
+
+            BlazorDiagram.Links.Add(Link1);
+            BlazorDiagram.Links.Add(Link2);
+
+            BlazorDiagram.Controls.AddFor(Link1)
+            .Add(new RemoveControl(new LinkPathPositionProvider(0.1))) ;
+
+                        BlazorDiagram.Controls.AddFor(Link2) 
+            .Add(new ArrowHeadControl(true))
+            .Add(new ArrowHeadControl(false));
         }
 
         protected void ToggleZoom() => BlazorDiagram.Options.Zoom.Enabled = !BlazorDiagram.Options.Zoom.Enabled;
@@ -46,9 +63,15 @@ namespace PUBGReddit.UGC.App.Pages
 
         private NodeModel NewNode(double x, double y)
         {
-            var node = new NodeModel(new Point(x, y));
-            node.AddPort(PortAlignment.Bottom);
-            node.AddPort(PortAlignment.Top);
+            var node = new AirDropNode(new Point(x, y)); 
+            node.AddPort(PortAlignment.Left);
+            node.AddPort(PortAlignment.Right);
+            return node;
+        }
+        
+          private NodeModel NewDeviceNode(double x, double y)
+        {
+            var node = new DeviceNode(new Point(x, y)); 
             node.AddPort(PortAlignment.Left);
             node.AddPort(PortAlignment.Right);
             return node;
