d08c29d4b2c2cdc582ec98722d9f8841d80a8d02
Delzaugh <Delzaugh@gmail.com>
2025-08-02 17:19:01 +0200

    cleanup

--- src/PUBGReddit.UGC.App/DiagramWidget/DeviceNodeNode.cs
+++ src/PUBGReddit.UGC.App/DiagramWidget/DeviceNodeNode.cs
@@ -1,15 +1,16 @@
 ﻿using Blazor.Diagrams.Core.Geometry;
 using Blazor.Diagrams.Core.Models;
 
-namespace PUBGReddit.UGC.App.Pages;
+namespace PUBGReddit.UGC.App.DiagramWidget;
 
-public class BotAnswerNode : NodeModel
+public class DeviceNode : NodeModel
 {
-    public BotAnswerNode(Point position, string deviceType) : base(position) { DeviceType = deviceType; }
+    public DeviceNode(Point position, string deviceType) : base(position) { DeviceType = deviceType; }
 
     public string DeviceType { get; private set; }
 
 }
+
 public class DevicePort : PortModel
 {
     public DevicePort(NodeModel parent, PortAlignment alignment, string label) : base(parent, alignment, null, null)
